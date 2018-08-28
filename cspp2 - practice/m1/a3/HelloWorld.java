b4a31e9a93a9dfc63875086138ec631d5464c424
pranay0124 <pranay0124@msitprogram.net>
2018-08-28 09:10:51 +0530

    a1-m3

---
+++ cspp2 - practice/m1/a3/HelloWorld.java
@@ -0,0 +1,23 @@
+
+/**
+    * Author : Pranay Kumar Y.
+    * Date : 27th august, 2018
+*/
+
+public final class HelloWorld {
+
+    /**.
+    * constructor
+    */
+    private HelloWorld() {
+        // Zero argument constructor...
+    }
+
+    /**
+    * @param args of type string[] : command line arguments.
+    *
+    */
+    public static void main(final String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
