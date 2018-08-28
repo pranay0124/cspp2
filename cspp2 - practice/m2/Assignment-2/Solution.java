55e2e88cff4665188be6eeea608a2bf4370c3c95
pranay0124 <pranay0124@msitprogram.net>
2018-08-28 20:46:01 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - practice/m2/Assignment-2/Solution.java
+++ cspp2 - practice/m2/Assignment-2/Solution.java
@@ -1,10 +1,33 @@
+/**
+ * Author : Pranay Kumar Y.
+ * Date : 28th August, 2018.
+ */
 import java.util.Scanner;
+/**
+ * Scanner class.
+ */
 import java.lang.Math;
+/**
+ * Math class
+ */
 
 public final class Solution {
-	/*
-	Do not modify this main function.
-	*/
+	/**
+	 * Constructor
+	 */
+	private Solution() {
+		// Zero Parameter Construction
+	}
+	
+	/**
+	 * Function for Quadratic Equation.
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 * @param      c     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public static double rootsOfQuadraticEquation(int a, int b, int c) {
         double sqrt = Math.sqrt((b * b) - (4 * a * c));
         double root1 = ((-b) + (sqrt)) / (2 * a);
@@ -12,6 +35,12 @@ public final class Solution {
 	    System.out.println(root1 + " " + root2);
 	    return 0;
 	}
+
+	/**
+	 * Main Function.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
