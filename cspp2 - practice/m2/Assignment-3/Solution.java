d5a41279f5a681f0cd7dc6314366c3d59983e9cf
pranay0124 <pranay0124@msitprogram.net>
2018-08-28 21:04:33 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 - practice/m2/Assignment-3/Solution.java
+++ cspp2 - practice/m2/Assignment-3/Solution.java
@@ -6,39 +6,41 @@ import java.util.Scanner;
 /**
  * Scanner class.
  */
-public class Solution {
-	/**
-	 * Constructor.
-	 */
-	private Solution() {
-		// Zero Parameter Constructor
-	}
-	
-	/**
-	 * Main Function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+public final class Solution {
+    /**
+     * Constructor.
+     */
+    private Solution() {
+        // Zero Parameter Constructor
+    }
+
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result = power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
-	}
+    }
 
-	/**
-	 * Power Function.
-	 *
-	 * @param      base      The base
-	 * @param      exponent  The exponent
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static long power(final int base, final int exponent) {
-		final int num = 1;
-		if (exponent == 0) {
-			return num;
-		} else return base * power(base, exponent - 1);
-	}
+    /**
+     * Power Function.
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static long power(final int base, final int exponent) {
+        final int num = 1;
+        if (exponent == 0) {
+            return num;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
 }
