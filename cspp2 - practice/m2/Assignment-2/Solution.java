442944a655662deb61bf713dc94c9bb6c05e4eb4
pranay0124 <pranay0124@msitprogram.net>
2018-08-28 20:47:15 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - practice/m2/Assignment-2/Solution.java
+++ cspp2 - practice/m2/Assignment-2/Solution.java
@@ -6,47 +6,43 @@ import java.util.Scanner;
 /**
  * Scanner class.
  */
-import java.lang.Math;
-/**
- * Math class
- */
-
 public final class Solution {
-	/**
-	 * Constructor
-	 */
-	private Solution() {
-		// Zero Parameter Construction
-	}
-	
-	/**
-	 * Function for Quadratic Equation.
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static double rootsOfQuadraticEquation(int a, int b, int c) {
-        double sqrt = Math.sqrt((b * b) - (4 * a * c));
+    /**
+     * Constructor
+     */
+    private Solution() {
+        // Zero Parameter Construction
+    }
+
+    /**
+     * Function for Quadratic Equation.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static double rootsOfQuadraticEquation(final int a,final int b,final int c) {
+        int z = 4;
+        double sqrt = Math.sqrt((b * b) - (z * a * c));
         double root1 = ((-b) + (sqrt)) / (2 * a);
         double root2 = ((-b) - (sqrt)) / (2 * a);
-	    System.out.println(root1 + " " + root2);
-	    return 0;
-	}
+        System.out.println(root1 + " " + root2);
+        return 0;
+    }
 
-	/**
-	 * Main Function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		Solution obj = new Solution();
-		obj.rootsOfQuadraticEquation(a, b, c);
-	}
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        Solution obj = new Solution();
+        obj.rootsOfQuadraticEquation(a, b, c);
+    }
 }
