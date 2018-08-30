4a2201c33f915833c82cd83df1da1ace064fefad
pranay0124 <pranay0124@msitprogram.net>
2018-08-30 19:37:01 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - practice/m4/Assignment-2/Solution.java
+++ cspp2 - practice/m4/Assignment-2/Solution.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 public class Solution {
-	public static int[][] matrix() {
-		Scanner s = new Scanner(System.in);
-		int row = s.nextInt();
-		int column = s.nextInt();
-		int[][] multi = new int[row][column];
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < i; j++) {
-				multi[i][j] = s.nextInt();
-			}
-		}
-		return multi;
-	}
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		Solution obj1 = new Solution();
-		int[][] matrix1 = obj1.matrix();
-        System.out.println(matrix1);
-	}
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int row1 = s.nextInt();
+        int column1 = s.nextInt();
+        int[][] matrix1 = new int[row1][column1];
+        for (int i = 0; i < row1; i++) {
+            for (int j = 0; j < column1; j++) {
+                matrix1[i][j] = s.nextInt();
+            }
+        }
+        
+        int row2 = s.nextInt();
+        int column2 = s.nextInt();
+        int[][] matrix2 = new int[row2][column2];
+        for (int k = 0; k < row2; k++) {
+            for (int l = 0; l < column2; l++) {
+                matrix2[k][l] = s.nextInt();
+            }
+        }
+
+        if (row1 == row2 && column1 == column2) {
+            int[][] matrix3 = new int[row2][column2];
+            String res = "";
+            for (int m = 0; m < row1; m++) {
+                for (int n = 0; n < column2; n++) {
+                    matrix3[m][n] = matrix1[m][n] + matrix2[m][n];
+                    res += matrix3[m][n] + " ";
+                }
+                res = res.trim() + "\n";
+            }
+            System.out.print(res);
+        } else {
+            System.out.println("not possible");
+        }
+    }
 }
