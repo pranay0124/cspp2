9747da6635455f3641516e52773d7d390b29eaa7
pranay0124 <pranay0124@msitprogram.net>
2018-08-30 14:40:00 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2 - practice/m4/Assignment-1/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Author : Pranay Kumar Y.
+ * Date : 30th August, 2018.
+ */
+import java.util.*;
+/**
+ * Class to find the maximum value in the array.
+ */
+public final class Solution {
+    /**
+     * Constructor.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
+
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] arrList = new int[size];
+        for (int i = 0; i < size; i++) {
+            arrList[i] = s.nextInt();
+        }
+        int max = arrList[0];
+        for (int j = 0; j < size; j++) {
+            if (max < arrList[j]) {
+                max = arrList[j];
+            }
+        }
+        System.out.println("Maximum : " + max);
+    }
+}
