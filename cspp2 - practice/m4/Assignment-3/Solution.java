23c0f6897022c1a780d8dad4ea9e6800dc1aceeb
pranay0124 <pranay0124@msitprogram.net>
2018-08-31 09:55:19 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2 - practice/m4/Assignment-3/Solution.java
+++ cspp2 - practice/m4/Assignment-3/Solution.java
@@ -1,24 +1,49 @@
-
+/**
+ * Author : Pranay Kumar Y.
+ * Date : 31st August, 2018.
+ */
 import java.util.Scanner;
+/**
+ * Class for converting the binary format to decimal.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i = 0; i <= n; i++) {
-			String s = sc.nextLine();
-			long res = binaryToDecimal(s); //Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	public static long binaryToDecimal(final String str) {
-		int len = str.length();
-		int sum = 0;
-		int j = 0;
-		for (int i = len - 1; i >= 0; i--) {
-			sum += Character.getNumericValue(str.charAt(i)) * (Math.pow(2, j));
-			j++;
-		}
-		return sum;
-	}
+    /**
+     * Constructor.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
 
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i <= n; i++) {
+            String s = sc.nextLine();
+            long res = binaryToDecimal(s); //Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+
+    /**
+     * Function for conversion.
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static long binaryToDecimal(final String str) {
+        int len = str.length();
+        int sum = 0;
+        int j = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            sum += Character.getNumericValue(str.charAt(i)) * (Math.pow(2, j));
+            j++;
+        }
+        return sum;
+    }
 }
