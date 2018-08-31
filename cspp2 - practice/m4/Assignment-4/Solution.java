70b0013b9c90a98417e97b05f971f9881f18111e
pranay0124 <pranay0124@msitprogram.net>
2018-08-31 10:18:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 - practice/m4/Assignment-4/Solution.java
+++ cspp2 - practice/m4/Assignment-4/Solution.java
@@ -1,24 +1,44 @@
-
+/**
+ * Author : Pranay Kumar Y.
+ * Date : 31st August, 2018.
+ */
 import java.util.Scanner;
-public class Solution {
-	/*
-	Do not modify the main function
-	*/
-	public static String reverseString(String str) {
-		int len = str.length();
-		String reverse = "";
-		for (int i = len - 1; i >= 0; i--) {
-			reverse = reverse + str.charAt(i);
-		}
-		return reverse;
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);
-		System.out.println(reverse);
+/**
+ * Class to reverse a string.
+ */
+public final class Solution {
+    /**
+     * Constructor.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
 
-	}
-	//Write reverseString function
+    /**
+     * Reverse Function.
+     *
+     * @param      str   The string
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(String str) {
+        int len = str.length();
+        String reverse = "";
+        for (int i = len - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        return reverse;
+    }
 
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
 }
