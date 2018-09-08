3d58e80892a7ba639019f3121eef95ce53826bf7
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-08 12:32:40 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2 - assignments/m13/Assignment-1/Solution.java
+++ cspp2 - assignments/m13/Assignment-1/Solution.java
@@ -34,18 +34,18 @@ class Set {
 		if (size == 0) {
 			return "{}";
 		}
-		String str = "{";
-		int i = 0;
-		for (i = 0; i < size - 1; i++) {
-			str = str + set[i] + ",";
-		}
-		str = str + set[i] + "}";
-		return str;
-		// String[] str = new String[size];
-		// for (int i = 0; i < size; i++) {
-  //           str[i] = Integer.toString(set[i]);
-  //       }
-  //       return Arrays.toString(str).replaceAll(" ", "");
+		// String str = "{";
+		// int i = 0;
+		// for (i = 0; i < size - 1; i++) {
+		// 	str = str + set[i] + ",";
+		// }
+		// str = str + set[i] + "}";
+		// return str;
+		String[] str = new String[size];
+		for (int i = 0; i < size; i++) {
+            str[i] = Integer.toString(set[i]);
+        }
+        return Arrays.toString(str).replaceAll(" ", "");
 	}
 
 	public void add(final int item) {
