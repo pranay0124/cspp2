19cd027cdc2241fe053c5a54b14fd2dc8ac6763a
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-14 19:37:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - assignments/m17/Shoppingcart/Solution.java
+++ cspp2 - assignments/m17/Shoppingcart/Solution.java
@@ -3,64 +3,70 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Zero Parameter Constructor.
-	}
-	public static void main(String[] args) {
-		ShoppingCart sc = new ShoppingCart();
-		Scanner scan = new Scanner(System.in);
-		boolean a = false;
-		int testcases = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < testcases; i++) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(" ");
-			switch (tokens[0]) {
-			case "Item":
-				String[] c1 = tokens[1].split(",");
-				Item item = new Item(c1[0], c1[1], c1[2]);
-				sc.addToCatalog(item);
-				break;
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
 
-			case "catalog":
-				sc.showCatalog();
-				break;
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        ShoppingCart sc = new ShoppingCart();
+        Scanner scan = new Scanner(System.in);
+        boolean a = false;
+        int testcases = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testcases; i++) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            switch (tokens[0]) {
+            case "Item":
+                String[] c1 = tokens[1].split(",");
+                Item item = new Item(c1[0], c1[1], c1[2]);
+                sc.addToCatalog(item);
+                break;
 
-			case "add":
-				String[] c2 = tokens[1].split(",");
-				sc.addToCart(new Item(c2[0], c2[1], null));
-				break;
+            case "catalog":
+                sc.showCatalog();
+                break;
 
-			case "show":
-				sc.showCart();
-				break;
+            case "add":
+                String[] c2 = tokens[1].split(",");
+                sc.addToCart(new Item(c2[0], c2[1], null));
+                break;
 
-			case "totalAmount":
-				System.out.println("totalAmount: " + sc.getTotalAmount());
-				break;
+            case "show":
+                sc.showCart();
+                break;
 
-			case "payableAmount":
-				System.out.println("Payable amount: " + sc.getPayableAmount());
-				break;
-			case "remove":
-				String[] c3 = tokens[1].split(",");
-				sc.removeFromCart(new Item(c3[0], c3[1], null));
-				break;
+            case "totalAmount":
+                System.out.println("totalAmount: " + sc.getTotalAmount());
+                break;
 
-			case "coupon":
-				if (a == false) {
-					if (sc.applyCoupon(tokens[1])) {
-						a = true;
-					}
-				}
-				break;
-			case "print":
-				sc.printInvoice();
-				break;
-			}
-		}
-	}
+            case "payableAmount":
+                System.out.println("Payable amount: " + sc.getPayableAmount());
+                break;
+            case "remove":
+                String[] c3 = tokens[1].split(",");
+                sc.removeFromCart(new Item(c3[0], c3[1], null));
+                break;
+
+            case "coupon":
+                if (a == false) {
+                    if (sc.applyCoupon(tokens[1])) {
+                        a = true;
+                    }
+                }
+                break;
+            case "print":
+                sc.printInvoice();
+                break;
+            }
+        }
+    }
 }
