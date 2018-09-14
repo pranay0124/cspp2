9e420bda0e7d329d40dc50a763e9815f91d93e58
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-14 19:36:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - assignments/m17/Shoppingcart/Solution.java
+++ cspp2 - assignments/m17/Shoppingcart/Solution.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
 
+/**
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//Zero Parameter Constructor.
+	}
 	public static void main(String[] args) {
 		ShoppingCart sc = new ShoppingCart();
 		Scanner scan = new Scanner(System.in);
