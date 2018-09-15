072dcbfe9b5a84bf1d5b454cc08bcd1302843a4e
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-15 12:21:08 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2 - assignments/m19/Assignment-1/Solution.java
+++ cspp2 - assignments/m19/Assignment-1/Solution.java
@@ -131,7 +131,7 @@ public final class Solution {
 		for (int i = 0; i < questionList.size(); i++) {
 			System.out.println(questionList.get(i).question + "(" + questionList.get(i).marks + ")");
 			for (int j = 0 ; j < questionList.get(i).choices.length; j++) {
-				System.out.print(questionList.get(i).choices[j] + "        ");
+				System.out.print(questionList.get(i).choices[j] + "	");
 			}
 			System.out.println();
 			System.out.println();
